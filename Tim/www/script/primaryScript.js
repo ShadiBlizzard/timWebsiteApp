b044722919d6252b9e8452455396fe91115df332
Miticosamu <sapuppo.samuel@gmail.com>
2016-07-06 09:53:07 +0200

    app

---
+++ Tim/www/script/primaryScript.js
@@ -0,0 +1,25 @@
+
+$(function primary() {
+
+    // load the header
+    $(".header").load( "header.html");
+	
+	// default behaviour: load homepage
+	$(".page").load( "home.html", function() {
+		
+		// after loading the page we should load the page manager for links (header + main div, footer doesn't contain links)
+		$.getScript('script/pageManager.js', function() {
+			// load dynamic links (index.html)
+			clickPageLinks();
+		});
+	});	
+		
+	
+	// load footer
+	$(".footer").load("footer.html"); 
+
+
+             
+});
+
+
